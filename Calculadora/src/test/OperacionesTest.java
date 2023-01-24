5f40ad1f355baf3784495b8b57bcd00eb3a66bac
agonsalez <a_gonzalez@fpzornotza.com>
2023-01-24 12:58:42 +0100

    "Añadido Javadoc"

---
+++ Calculadora/src/test/OperacionesTest.java
@@ -0,0 +1,55 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import operaciones.Operaciones;
+
+class OperacionesTest {
+
+	@Test
+	void testSumar() {
+		Operaciones operaciones = new Operaciones();		//Ctrl + Mayus + O
+		int numero1=5, numero2=3;
+		int resultado = operaciones.sumar(numero1, numero2);
+		assertEquals(resultado, 8);
+	}
+
+	@Test
+	void testRestar() {
+		Operaciones operaciones = new Operaciones();
+		int numero1=5, numero2=3;
+		int resultado = operaciones.restar(numero1, numero2);
+		assertEquals(resultado, 2);
+		
+	}
+
+	@Test
+	void testMultiplicar() {
+		Operaciones operaciones = new Operaciones();
+		int numero1=5, numero2=3;
+		int resultado = operaciones.multiplicar(numero1, numero2);
+		assertEquals(resultado, 15);
+
+	}
+
+	@Test
+	void testDividir() {
+		Operaciones operaciones = new Operaciones();
+		int numero1=5, numero2=3;
+		int resultado = operaciones.dividir(numero1, numero2);
+		assertEquals(resultado, 1);
+
+	}
+
+	@Test
+	void testResto() {
+		Operaciones operaciones = new Operaciones();
+		int numero1=5, numero2=3;
+		int resultado = operaciones.resto(numero1, numero2);
+		assertEquals(resultado, 2);
+
+	}
+
+}
